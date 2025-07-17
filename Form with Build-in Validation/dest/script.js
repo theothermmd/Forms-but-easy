c8a3e7ab4db6387a57459324af8ab3f6c668f560
Mohamadreza <161870762+theothermmd@users.noreply.github.com>
2025-07-17 10:33:00 +0330

    Add files via upload

---
+++ Form with Build-in Validation/dest/script.js
@@ -0,0 +1,17 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", () => {
+    // This rrow function will run after created DOM and render is finished.
+    const form = document.getElementById("myform"); // selecet our form with ID
+    /* What is HTMLFormElement for?
+    When you select an element, TypeScript defaults to HTMLElement, but we need the HTMLFormElement type to create FormData. */
+    form.addEventListener("submit", (event) => {
+        // This arrow function is executed when the submit button is clicked.
+        event.preventDefault(); // Prevents the browser's default behavior of refreshing the page.
+        const formdata = new FormData(form);
+        const data = {};
+        formdata.forEach((value, key) => {
+            data[key] = value.toString();
+        });
+        console.log(JSON.stringify(data));
+    });
+});
